abd1d3f861360f5b6da945a66cfd9591b3eb8a4d
Eric Boyd <eric.hcr80@gmail.com>
2017-12-09 17:44:09 -0600

    Adds: Movement buffer, handling of XY movements and M3/M4 for servo lift.

--- min-vplot.ino
+++ min-vplot.ino
@@ -1,11 +1,15 @@
+#include <RBD_Servo.h>
+
 #include "uStepper.h"
 #include "TimerOne.h"
 #include "grbl_read_float.h"
 
-uStepper motor( 80, 0, 10000, 11, 12, 2);
-uStepper motor2( 80, 1, 10000, 10, 13, 2);
+uStepper motor( 80, 0, 20000, 4, 3, 6);
+uStepper motor2( 80, 1, 20000, 8, 7, 6);
 
-float stepper_distance = 500; // mm
+RBD::Servo servo(2, 1000, 2750);
+
+float stepper_distance = 1000; // mm
 
 float origin_x = stepper_distance / 2.0;
 float origin_y = stepper_distance / 2.0;
@@ -26,36 +30,140 @@ float b_length_from_xy(float x, float y)
   return sqrt(dx * dx + dy * dy);
 }
 
-float dlength_to_steps(float dlength)
-{
-  return dlength / .188496 * 16; // mm / step
+long length_to_steps(float length)
+{ 
+  return length / .188496 * 16; // mm / step
 }
-
-float a = stepper_distance * sqrt(2) / 2;
-float b = stepper_distance * sqrt(2) / 2;
-
-float x = 0.0;
-float y = 0.0;
 
 float da = 0.0;
 float db = 0.0;
 
-float max_speed = 200;
+float max_speed = 800;
 
-float a_speed = 0;
+float a_speed = 0; 
 float b_speed = 0;
 
-volatile long a_dest = 0; // 200L * 16L * 2L;
-volatile long b_dest = 0; // 200L * 16L;
+volatile long a_dest = 0;
+volatile long b_dest = 0;
+
+class gc_block
+{
+public:
+  float x = 0.0;
+  float y = 0.0;
+
+  bool lift = true;
+  bool ack = false;
+
+  gc_block() {}
+  gc_block(float _x, float _y, bool _lift) : x(_x), y(_y), lift(_lift) {}
+};
+
+class gc_state
+{
+public:
+  int feed = 0;
+    
+  bool rapid = false;
+
+  float x = 0.0;
+  float y = 0.0;
+
+  float a_length = 0.0;
+  float b_length = 0.0;
+  
+  long a_steps = 0L;
+  long b_steps = 0L;
+    
+  bool lift = true;
+
+  gc_state()
+  {
+    a_length = stepper_distance * sqrt(2) / 2;
+    b_length = stepper_distance * sqrt(2) / 2;   
+
+    a_steps = length_to_steps(a_length);
+    b_steps = length_to_steps(b_length);
+
+    a_dest = a_steps;
+    b_dest = b_steps;
+  }
+};
+
+#define BUFFER_SIZE 5
+gc_block gc_buffer[BUFFER_SIZE];
+
+int buffer_front = 0;
+int buffer_back = 0;
+
+int get_buffer_front()
+{
+  return buffer_front;
+}
+
+bool get_buffer_empty()
+{
+  return buffer_front == buffer_back;
+}
+
+bool get_buffer_full()
+{
+  return buffer_front == get_next();
+}
+
+int get_next()
+{
+  if (buffer_back >= BUFFER_SIZE - 1)
+    return 0;
+  else
+    return buffer_back + 1;
+}
+
+gc_block buffer_advance();
+gc_block buffer_advance()
+{
+  int current = buffer_front;
+  buffer_front++;
+  if (buffer_front >= BUFFER_SIZE)
+    buffer_front = 0;
+  return gc_buffer[current];
+}
+
+gc_block & buffer_current();
+gc_block & buffer_current()
+{
+  return gc_buffer[buffer_front];
+}
+
+gc_block & buffer_last();
+gc_block & buffer_last()
+{
+  return gc_buffer[buffer_back - 1 < 0 ? BUFFER_SIZE - 1 : buffer_back - 1];
+}
+
+bool buffer_add(gc_block gc_add);
+bool buffer_add(gc_block gc_add)
+{
+  if (get_buffer_full())
+    return false;
+
+  int next = buffer_back;
+  gc_buffer[next] = gc_add;
+  
+  buffer_back = get_next();
+  
+  return true;
+}
+
+gc_state current_state;
 
 void do_move(float new_x, float new_y)
 {
   noInterrupts();
-  Timer1.stop();
-  
+
   bool log_debug = false;
 
-  if (log_debug)
+  if (log_debug && false)
   {
     Serial.print("New position: ");
     Serial.print(new_x);
@@ -74,8 +182,8 @@ void do_move(float new_x, float new_y)
     Serial.println(new_b);
   }
 
-  da = new_a - a;
-  db = new_b - b;
+  da = new_a - current_state.a_length;
+  db = new_b - current_state.b_length;
 
   if (log_debug)
   {
@@ -85,25 +193,30 @@ void do_move(float new_x, float new_y)
     Serial.println(db);
   }
 
-  a = new_a;
-  b = new_b;
+  current_state.a_length = new_a;
+  current_state.b_length = new_b;
 
-  x = new_x;
-  y = new_y;
+  current_state.x = new_x;
+  current_state.y = new_y;
 
-  float steps_a = dlength_to_steps(da);
-  float steps_b = dlength_to_steps(db);
+  long new_a_steps = length_to_steps(new_a);
+  long new_b_steps = length_to_steps(new_b);
+
+  if (current_state.a_steps - new_a_steps == 0 && current_state.b_steps - new_b_steps == 0)
+  {
+    return;
+  }
+
+  a_dest = new_a_steps;
+  b_dest = new_b_steps;
 
   if (log_debug)
   {
-    Serial.print("Change length (steps): ");
-    Serial.print(steps_a);
+    Serial.print("Step dest: ");
+    Serial.print(a_dest);
     Serial.print(" ");
-    Serial.println(steps_b);
+    Serial.println(b_dest);
   }
-
-  a_dest = steps_a + motor.getPositionSteps();
-  b_dest = steps_b + motor2.getPositionSteps();
   
   a_speed = max_speed;
   b_speed = max_speed;
@@ -130,23 +243,47 @@ void do_move(float new_x, float new_y)
     Serial.print(" B speed: " );
     Serial.println(b_speed);
   }
+//
+//  if (log_debug)
+//  {
+//    Serial.print("Buffer: ");
+//    Serial.print(buffer_front);
+//    Serial.print(" ");
+//    Serial.println(buffer_back);
+//  }
 
- Timer1.resume();
  interrupts();
+}
+
+int lift_delay = 100;
+int lift_position = 90;
+
+void do_lift(bool lift)
+{
+  bool log_debug = false;
+
+  if (log_debug)
+  {
+    Serial.print("Servo: ");
+    Serial.println(lift ? lift_position : 0);
+  }
+
+  current_state.lift = lift;
+  servo.moveToDegrees(lift ? lift_position : 0);
 }
 
 static char line[64];
 
 void parse_line()
-{
-  Serial.print("Parsing line:");
-  Serial.println(line);
-  
+{ 
   uint8_t char_counter = 0;
+  bool movement = false;
 
+  gc_block current_block = buffer_last();
+  
   while (line[char_counter] != 0)
   {
-    if (line[char_counter] == ' ')
+    if (line[char_counter] == ' ' || line[char_counter] == '\r' || line[char_counter] == '\n')
     {
       char_counter++;
       continue;
@@ -158,22 +295,133 @@ void parse_line()
     float value = 0.0;
     if (read_float(line, &char_counter, &value))
     {
-      Serial.print("Letter: ");
-      Serial.print(letter);
-      Serial.print(", Value: " );
+      if (value > 1e9 || value < -1e9 || value == NAN)
+      {
+      Serial.print("Parsing error: ");
       Serial.println(value);
+      }
+      
+      if (letter == 'G')
+      {
+        if (value == 1)
+        {
+          current_state.rapid = true;
+        }
+        else if (value == 0)
+        {
+          current_state.rapid = false;
+        }
+      }
+      else if (letter == 'M')
+      {
+        if (value == 0.0)
+        {
+          Serial.print(motor.getPositionSteps());
+          Serial.print(" ");
+     
+          Serial.print(a_dest);
+          Serial.print(" ");    
+          Serial.print(a_speed);
+          Serial.print(" ");  
+          Serial.print(motor2.getPositionSteps());
+          Serial.print(" ");
+          Serial.print(b_dest);
+          Serial.print(" "); 
+          Serial.print(b_speed);
+          Serial.print(" ");
+          Serial.println(get_buffer_empty());
+        }
+        else
+        {
+//          Serial.print("parsed lift");
+//          Serial.print(buffer_front);
+//          Serial.print(" ");
+//          Serial.print(current_block.x);
+//          Serial.print(" ");
+//          Serial.println(current_block.y);
+          
+          current_block.lift = value == 3;
+        }
+      }
+      else if (letter == 'F')
+      {
+        current_state.feed = (int)value;
+      }
+      else if (letter == 'X')
+      {
+        current_block.x = value;
+      }
+      else if (letter == 'Y')
+      {
+        current_block.y = value;
+      }
+          
       continue;
     }
     else
     {
-      Serial.println("Not a float");
+      Serial.println("Invalid number");
+      return;
     }   
 
     char_counter++;
   }
+
+  if (!get_buffer_full())
+  {
+    buffer_add(current_block);
+    
+    if (!get_buffer_full())
+    {
+      Serial.println("ok");
+    }
+  }
+  else
+  {
+    Serial.println("dropped");
+    
+  }
 }
 
-void receive_loop()
+void run_steppers()
+{
+  if (motor.getPositionSteps() == a_dest && motor2.getPositionSteps() == b_dest)
+  {    
+    if (!get_buffer_empty())
+    {
+      bool was_full = get_buffer_full();
+      gc_block block = buffer_advance();
+
+      if (block.x != current_state.x ||
+          block.y != current_state.y)
+      {
+//          Serial.print("start move");
+//          Serial.print(block.x);
+//          Serial.print(block.y);
+//          Serial.print(" ");
+//          Serial.println(get_buffer_empty());
+//          Serial.flush();            
+         do_move(block.x, block.y);
+      }
+
+      if (block.lift != current_state.lift)
+      {
+        //Serial.println("start lift");
+       // Serial.flush();
+        
+        do_lift(block.lift); 
+        delay(1000);
+      }
+
+      if (was_full)
+        Serial.println("ok");        
+    }  
+
+    
+  }
+}
+
+void loop()
 {
   char c;
   uint8_t char_counter = 0;
@@ -185,54 +433,26 @@ void receive_loop()
       if ((c == '\n') || (c == '\r')) // End of line reached
       { 
         line[char_counter] = 0; // Set string termination character.
-        char_counter = 0;
 
-        parse_line();
+        if (char_counter > 0) 
+          parse_line();
+        
+        char_counter = 0; 
+        line[0] = 0;
       }
       else 
       {
-        line[char_counter++] = c;   
+        line[char_counter++] = c;
       }
     }
+  
+    run_steppers();
 
-    /*
-    long m1steps = motor.getPositionSteps();
-    long m2steps = motor2.getPositionSteps();
-    
-    bool motor_1_running = m1steps != a_dest;
-    bool motor_2_running = m2steps != b_dest;
-    */
-
-    
+    Serial.flush();
   }
 }
 
-void setup()
-{
-  Serial.begin(9600);
-  // put your setup code here, to run once:
-
-  motor.enable();
-  motor.setSpeed(0);
-
-  motor2.enable();
-  motor2.setSpeed(0);
-
-  Serial.print("Ready: ");
-
-  Timer1.initialize(100); // set a timer of length 100 microseconds (or 0.1 sec - or 10Hz => the led will blink 5 times, 5 cycles of on-and-off, per second)
-  Timer1.attachInterrupt( my_loop ); // attach the service routine here
-
-  receive_loop();
-}
-
-bool has_direction = false;
-bool has_amount = false;
-
-int incoming_direction = 0;
-int incoming_amount = 0;
-
-void my_loop()
+void stepper_isr()
 {
   if (motor.getPositionSteps() != a_dest)
     motor.step();
@@ -240,68 +460,49 @@ void my_loop()
   if (motor2.getPositionSteps() != b_dest)
     motor2.step();
 
-  // Serial.println(motor_1_running);
-  // Serial.println(motor_2_running);
-
-  /*
-    Serial.print("Step1:");
-    Serial.print(motor.getPositionSteps());
-    Serial.print("\tStep2:");
-    Serial.println(motor2.getPositionSteps());
-  */
-
-  // send data only when you receive data:
-
-  /*
-  if (Serial.available() > 0)
-  {
-    byte val = Serial.read();
-
-    if (!has_direction)
-    {
-      incoming_direction = val - 48;
-      has_direction = true;
-    }
-    else
-    {
-      incoming_amount = val - 48;
-
-      Serial.print("I received: ");
-      Serial.print(incoming_direction, DEC);
-      Serial.print(" ");
-      Serial.println(incoming_amount, DEC);
-
-      has_amount = false;
-      has_direction = false;
-
-      if (incoming_direction == 1)
-      {
-        motor.setSpeed(max_speed);
-        a_dest += incoming_amount * 1000;
-      }
-      else if (incoming_direction == 2)
-      {
-        motor.setSpeed(-max_speed);
-        a_dest -= incoming_amount * 1000;
-      }
-      else if (incoming_direction == 3)
-      {
-        motor2.setSpeed(max_speed);
-        b_dest += incoming_amount * 1000;
-      }
-      else if (incoming_direction == 4)
-      {
-        motor2.setSpeed(-max_speed);
-        b_dest -= incoming_amount * 1000;
-      }
-      else if (incoming_direction == 5)
-      {
-        drawing_box = true;
-
-        motor.setPosition(0);
-        motor2.setPosition(0);
-      }
-    }
-  }
-  */
+   servo.update();
 }
+
+void setup()
+{
+ /*
+  Serial.println(get_buffer_empty());
+  Serial.println(buffer_add(gc_block(25.0, 25.0, true)));
+  Serial.println(buffer_add(gc_block(-25.0, 25.0, true)));
+  Serial.println(buffer_add(gc_block(-25.0, -25.0, true)));
+  Serial.println(get_buffer_full());
+  Serial.println(buffer_add(gc_block(25.0, -25.0, true)));
+  Serial.println(get_buffer_full());
+  Serial.println(buffer_add(gc_block(50.0, 50.0, true)));
+  Serial.println(get_buffer_full());
+  */
+
+  Timer1.initialize(50);
+  Timer1.attachInterrupt(stepper_isr);
+
+  servo.moveToDegrees(0);
+  delay(1000);
+  servo.moveToDegrees(lift_position);
+
+  motor.setSpeed(0);
+  motor2.setSpeed(0);   
+
+  Serial.begin(115200);
+  Serial.setTimeout(0);
+  
+  while (!Serial)
+    delay(10); // for micro  
+
+  Serial.print("Ready\r\n");
+
+  motor.setPosition(current_state.a_steps);
+  motor2.setPosition(current_state.b_steps);
+
+  Serial.print(current_state.a_steps);
+  Serial.print(" ");
+  Serial.println(current_state.b_steps);
+
+  motor.enable();
+  motor2.enable();
+}
+
